977f2adda69cab759202e2fb1d51184c25809b10
Techwhizz47 <allanndungu47@gmail.com>
2024-11-08 11:26:55 +0300

    updates

--- index.mjs
+++ index.mjs
@@ -22,6 +22,16 @@ app.get('/about', (req, res) => {
 app.get('/contact', (req, res) => {
     res.render("contact.ejs");
 })
+app.get('/droneview', (req, res) => {
+    res.render("page1.ejs");
+})
+app.get('/wetland', (req, res) => {
+    res.render("page2.ejs");
+})
+
+
+
+
 
 
 app.listen(port, ()=>{
